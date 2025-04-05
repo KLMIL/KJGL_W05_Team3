360fd5d37851f73d507f8075bc81daedaf4e7a52
pdefd77 <pdefd77@gmail.com>
2025-04-05 16:47:40 +0900

    [feat] Shelter 들어오면 들고 있던 인터랙트 부수고 UI 갱신하는 기능 추가

--- Assets/_SDH/Scripts/DisassembleSystem.cs
+++ Assets/_SDH/Scripts/DisassembleSystem.cs
@@ -2,13 +2,26 @@ using UnityEngine;
 
 public class DisassembleSystem
 {
-    public IngredientTuple[] Disassemble(GameObject interactable)
+    public InteractableSO Disassemble(GameObject item)
     {
-        string id = interactable?.GetComponent<Interactable>()?.Id;
-
-        if (id == null)
+        if (!item)
         {
-            Debug.Log("notem");
+            Debug.Log("No item");
+            return null;
         }
+
+        string id = item.GetComponent<Interactable>().Id;
+
+        InteractableSO interactable = DatabaseManager.Instance.GetInteractable(id);
+
+        if (interactable == null)
+        {
+            Debug.Log("Wrong interactable id");
+            return null;
+        }
+
+        Object.Destroy(item);
+
+        return interactable;
     }
 }
